d00ac4669374cfe4976e887fa7c1ac36ba9459d2
iremkaraoglu <iremk97@gmail.com>
2022-05-16 13:44:34 +0300

    support other iphone models

--- code.js
+++ code.js
@@ -22,25 +22,67 @@ function runPlugin() {
             Width: "750",
             Height: "1334",
             Scale: "2",
-            Models: ["iPhone SE 2nd gen"],
+            Models: ["iPhone SE 2nd gen", "iPhone 8", "iPhone 7", "iPhone 6S"],
         },
         "1242-2688": {
             Width: "1242",
             Height: "2688",
             Scale: "3",
-            Models: ["iPhone 11 Pro Max"],
+            Models: ["iPhone 11 Pro Max", "iPhone XS Max"],
         },
         "1125-2436": {
             Width: "1125",
             Height: "2436",
             Scale: "3",
-            Models: ["iPhone 11 Pro"],
+            Models: ["iPhone 11 Pro", "iPhone XS", "iPhone X"],
         },
         "828-1792": {
             Width: "828",
             Height: "1792",
             Scale: "2",
-            Models: ["iPhone 11, iPhone XR"],
+            Models: ["iPhone 11", "iPhone XR"],
+        },
+        "1080-1920": {
+            Width: "1080",
+            Height: "1920",
+            Scale: "3",
+            Models: ["iPhone 8 Plus"],
+        },
+        "1242-2208": {
+            Width: "1242",
+            Height: "2208",
+            Scale: "3",
+            Models: ["iPhone 7 Plus", "iPhone 6S Plus", "iPhone 6 Plus"],
+        },
+        "640-1136": {
+            Width: "640",
+            Height: "1136",
+            Scale: "2",
+            Models: ["iPhone SE 1st gen", "iPhone 5C"],
+        },
+        "750-1136": {
+            Width: "750",
+            Height: "1136",
+            Scale: "2",
+            Models: ["iPhone 6"],
+        },
+        "640-960": {
+            Width: "640",
+            Height: "960",
+            Scale: "2",
+            Models: ["iPhone 5S", "iPhone 5", "iPhone 4S", "iPhone 4"],
+        },
+        "320-480": {
+            Width: "320",
+            Height: "480",
+            Scale: "1",
+            Models: ["iPhone 3GS", "iPhone 3G", "iPhone 1st gen"],
+        },
+        "1488-2266": {
+            Width: "1488",
+            Height: "2266",
+            Scale: "2",
+            Models: ["iPad Mini (6th gen)"],
         },
     };
     for (const node of figma.currentPage.selection) {
@@ -55,12 +97,20 @@ function runPlugin() {
                 figma.closePlugin("Nothing applied.");
             }
             else {
-                console.log(devices[key]);
-                console.log(devices[key].Scale);
                 const scale = Number(devices[key].Scale);
                 layer.rescale(1 / scale);
-                figma.closePlugin("The selected frame downscaled for 1/" + scale);
+                if (scale == 1) {
+                    figma.closePlugin("Scale factor is 1, no changes");
+                }
+                else {
+                    figma.closePlugin("The selected frame downscaled for 1/" + scale);
+                }
             }
+        }
+        else {
+            const scale = Number(devices[key].Scale);
+            layer.rescale(1 / scale);
+            figma.closePlugin("The selected frame downscaled for 1/" + scale);
         }
     }
     figma.closePlugin();
